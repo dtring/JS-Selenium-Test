0cc4514c3bf145e2175f44dd8b3b9c08170a47f1
dtring <49992154+dtring@users.noreply.github.com>
2021-04-14 08:47:47 +0100

    commit cupdate to selenium

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    await driver.sleep(5000);
+    await driver.get('http://bidjs.com');
+    await driver.sleep(2000);
+    await driver.findElement(By.linkText('demo')).click();
+    await driver.sleep(2000);
+    await driver.wait(until.titleIs('Upcoming Auctions | Demonstration Auctions'), 1000);
+    await (await driver.findElement(By.linkText('login'))).click();
+    await driver.wait(until.titleIs('Log In | Demonstration Auctions'), 1000);
+    await driver.findElement(By.id('identifier')).sendKeys('admin_demo');
+    await driver.findElement(By.id('password')).sendKeys('5unSh1ne');
+    await (await driver.findElement(By.id('submit'))).click();
+    await driver.sleep(2000);
+
   } finally {
     await driver.quit();
   }
